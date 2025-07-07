cb7c87e91ad3be564a3497e64162a1bee1f19188
peyush <piyushnawal19.com>
2025-07-07 16:43:32 +0530

    "day -2 calculating expression without the help of eval"

---
+++ day-2/CalculateExpressionWIthoutEvalFunc.js
@@ -0,0 +1,46 @@
+  
+// calculating expression without the help of eval
+
+
+function calculateExpression(expression) {
+    const stack = [];
+    let currentNumber = 0
+
+    let operation = '+'
+    const n = expression.length;
+       
+    for (let i = 0; i < n; i++) {
+        const char = expression[i]
+   
+        if (/\d/.test(char)) {
+            currentNumber = currentNumber * 10 + Number(char)
+        }
+
+        if (/[+\-*]/.test(char) || i === n - 1) {
+            switch (operation) {
+                case "+":
+                    stack.push(currentNumber);
+                    break;
+                case "-":
+                    stack.push(-currentNumber);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * currentNumber);
+                    break;
+                case "/":
+                    stack.push(Math.trunc(stack.pop() / currentNumber));
+                    break;
+            }
+            currentNumber = 0;
+            operation = char;
+        }
+    }
+      
+    
+    return stack.reduce((acc, num) => acc + num, 0);
+}
+  
+
+
+
+console.log("Output:",calculateExpression("3+23*2"));// Output: 7
