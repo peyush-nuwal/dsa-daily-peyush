6efde7cc031a38739d190b807e21fe7b825c39bd
Peyush Nuwal <piyushnawal19@gmail.com>
2025-07-10 19:29:46 +0530

    problem-2 duplicated items in array

---
+++ day-5/findDuplicates.js
@@ -0,0 +1,36 @@
+function findDuplicates(nums) {
+    const result = [];
+    
+    
+    for (let i = 0; i < nums.length; i++){
+        const value = nums[i]
+        console.log("current index ", i,"ki value", value)
+        if (value === undefined) continue;
+
+        let index = Math.abs(value);
+        console.log("value of positve kiya",index)
+        if (index === undefined) continue;
+        
+        console.log("checking if its lower then 0 or not")
+        console.log(nums[index] < 0)
+        if (nums[index] < 0) {
+           
+
+            if (!result.includes(index)) {
+                 result.push(index)
+            }
+            
+        }
+        else {
+            console.log("passing negtive value to mark we been here")
+            console.log("nums[index]= -nums[index]")
+            console.log(nums[index] , -nums[index])
+            nums[index] = -nums[index];
+        }
+        
+    }
+    return result
+}
+  
+  
+console.log(findDuplicates([3, 1, 3, 4,2]))
