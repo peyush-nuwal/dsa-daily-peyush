1f6146f4a9322cdf96376d1c196882bf34eb041c
Peyush Nuwal <piyushnawal19@gmail.com>
2025-07-17 00:26:14 +0530

    day-7 problem 3 : kth number

---
+++ day-7/kthNum.js
@@ -0,0 +1,14 @@
+let arr = [7, 10, 4, 3, 20, 15], k =2
+ 
+function kthSmallest(arr, k) {
+    if (k > 0 && k <= arr.length + 1) {
+        arr.sort((a, b) => a - b)
+        return arr[k - 1]
+    }
+    return -1
+}
+
+
+console.log(kthSmallest(arr, k)) // 7
+console.log(arr.sort((a, b) => a - b));
+console.log(arr.sort((a, b) => a + b));
