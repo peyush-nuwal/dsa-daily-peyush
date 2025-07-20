59ec614166f152aa5b78e73e4353608f2908fa9e
Peyush Nuwal <piyushnawal19@gmail.com>
2025-07-20 21:16:28 +0530

    day-10: problem 2 - shift by k num better time complexity

--- day-9/shiftByKnum.js
+++ day-9/shiftByKnum.js
@@ -1,14 +1,25 @@
+ //brute force
+// let k = 7
+// const arr = [1, 2, 3, 4, 5]
 
-let k = 2
-const arr = [1, 2, 3, 4, 5]
+// for (let index = 0; index < k%arr.length; index++) {
+  
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
 
-for (let index = 0; index < k; index++) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
-
-    }
+//     }
     
+// }
+
+
+// console.log(arr);
+
+
+let k=3
+const arr = [1, 2, 3, 4, 5]
+const temp = new Array(arr.length)
+
+for (let i = 0; i < arr.length; i++){
+    temp[i] = arr[(i + k) % arr.length]
 }
-
-
-console.log(arr);
+console.log(temp)
