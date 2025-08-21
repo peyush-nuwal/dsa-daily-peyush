46b7f28037fc70579a01d018d38b812777d5a0f0
Peyush Nuwal <piyushnawal19@gmail.com>
2025-08-21 11:29:12 +0530

    added deletebyIndex function

--- dsa-conncept/Arrays/customArray.js
+++ dsa-conncept/Arrays/customArray.js
@@ -29,17 +29,45 @@ class myArray{
         this.length--
         return firstItem
     }
+    deleteByIndex(idx) {
+        const itemToDelete = this.data[idx]
+        for (let i = idx; i < this.length; i++){
+            this.data[i]=this.data[i+1]
+        }
+        delete this.data[this.length-1]
+        this.length--
+        return itemToDelete
+    }
 }
 
 
 const myNewArr = new myArray()
+
 console.log(myNewArr);
+
+// adding element in array
 myNewArr.add(1)
 myNewArr.add(2)
 myNewArr.add(3)
-// myNewArr.get(1)
+myNewArr.add(4)
+myNewArr.add(5)
+myNewArr.add(6)
+
 console.log(myNewArr);
+
+// get element by index
+// myNewArr.get(1)
+
+//delete element from end
 // console.log(myNewArr.pop());
 // console.log(myNewArr);
-console.log(myNewArr.shift());
-console.log(myNewArr);
+
+//delete element from start
+// console.log(myNewArr.shift());
+// console.log(myNewArr);
+
+//delete element by index 
+console.log("delete element at index",myNewArr.deleteByIndex(1))
+console.log(myNewArr)
+console.log("delete element at index",myNewArr.deleteByIndex(4))
+console.log(myNewArr)
