de61c57929c68457adbb351d009b006de35126b6
Peyush Nuwal <piyushnawal19@gmail.com>
2025-08-21 11:07:10 +0530

    added delete method

--- dsa-conncept/Arrays/customArray.js
+++ dsa-conncept/Arrays/customArray.js
@@ -13,6 +13,12 @@ class myArray{
 
       console.log(idx,"at index",this.data[idx])
     }
+
+    pop() {
+        delete this.data[this.length - 1]
+        this.length--
+       return this.data
+    }
     
 }
 
@@ -23,4 +29,6 @@ myNewArr.add(2)
 myNewArr.add(1)
 myNewArr.add(3)
 myNewArr.get(1)
+console.log(myNewArr);
+console.log(myNewArr.pop());
 console.log(myNewArr);
