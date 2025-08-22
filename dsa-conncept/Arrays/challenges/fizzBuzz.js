732b5b9e9e588ea5f9ef844588d5717619351ccc
Peyush Nuwal <piyushnawal19@gmail.com>
2025-08-22 14:08:33 +0530

    array cahllenge - 5: fizzbuzz

---
+++ dsa-conncept/Arrays/challenges/fizzBuzz.js
@@ -0,0 +1,31 @@
+/*
+Print numbers from 1 to n.
+- Multiples of 3: "Fizz"
+- Multiples of 5: "Buzz"
+- Multiples of both: "FizzBuzz"
+*/
+
+
+
+const FizzBuzz = (n) => {
+       const res=[]
+     
+    for (let i = 1; i <= n; i++){
+        if (i % 3 === 0 && i % 5 === 0) {
+             res[i-1]="FizzBuzz"
+        }
+        else if ( i % 3 === 0) {
+            res[i-1] = "Fizz"
+        }
+        else if ( i % 5 === 0) {
+            res[i-1] = "Buzz"
+        }
+        else {
+            res[i-1]=i
+        }
+    }
+    return res
+}
+
+
+console.log(FizzBuzz(15));
