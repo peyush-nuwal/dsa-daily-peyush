6e981623ff4f6bdaffa0270520266721af77a96d
Peyush Nuwal <piyushnawal19@gmail.com>
2025-08-23 12:25:10 +0530

    added push and printlist

--- dsa-conncept/LinkedList/linkedlist.js
+++ dsa-conncept/LinkedList/linkedlist.js
@@ -2,7 +2,7 @@
 class Node{
 
     constructor(value) {
-        this.head = value
+        this.value = value
         this.next=null
     }
 }
@@ -14,9 +14,47 @@ class linkedList{
         this.tail = this.head
         this.length=1
     }
+
+    push(value) {
+        const newNode = new Node(value)
+        this.tail.next = newNode
+        this.tail=newNode
+        this.length++
+        
+    }
+    printList() {
+        let current = this.head;
+        let result = [];
+
+        while (current) {
+            result.push(current.value);
+            current=current.next
+        }
+        console.log(result.join(" -> "));
+    }
+
+    find(target) {
+        let current = this.head;
+        let res = []
+        
+        while (current) {
+           
+            if (target === current.value) {
+                res.push(current)
+            }
+            current = current.next
+        }
+        return res
+    }
 }
 
 
 const myLinkedList = new linkedList(1)
-
-console.log(myLinkedList);
+console.log("before insertaion", myLinkedList);
+myLinkedList.push(2)
+myLinkedList.push(3)
+myLinkedList.push(4)
+myLinkedList.push(5)
+console.log("after insertaion", myLinkedList);
+myLinkedList.printList()
+console.log("fined",myLinkedList.find(2));
