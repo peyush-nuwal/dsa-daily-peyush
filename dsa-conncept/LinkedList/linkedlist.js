fae9ad1cc6d19210a348311c1f8a4503098f628f
Peyush Nuwal <piyushnawal19@gmail.com>
2025-08-23 18:01:35 +0530

    linked list : pop() delete last element

--- dsa-conncept/LinkedList/linkedlist.js
+++ dsa-conncept/LinkedList/linkedlist.js
@@ -10,18 +10,38 @@ class Node{
 class linkedList{
     constructor(value){
 
-        this.head = new Node(value);
+        this.head =  new Node(value)
         this.tail = this.head
-        this.length=1
+        this.length =  1
     }
 
     push(value) {
         const newNode = new Node(value)
-        this.tail.next = newNode
-        this.tail=newNode
+        if (!this.head) {  // if list empty
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
         this.length++
         
     }
+    pop() {
+        let temp = this.head
+        let prev = this.head
+        while (temp.next) {
+            prev = temp
+            temp=prev.next
+        }
+        this.tail = prev
+        this.tail.next = null
+        this.length--
+        return temp
+     }
+
+ 
+
     printList() {
         let current = this.head;
         let result = [];
@@ -32,7 +52,7 @@ class linkedList{
         }
         console.log(result.join(" -> "));
     }
-
+     
     find(target) {
         let current = this.head;
         let res = []
@@ -46,15 +66,18 @@ class linkedList{
         }
         return res
     }
+    
 }
 
 
 const myLinkedList = new linkedList(1)
-console.log("before insertaion", myLinkedList);
+// console.log("before insertaion", myLinkedList);
 myLinkedList.push(2)
 myLinkedList.push(3)
 myLinkedList.push(4)
 myLinkedList.push(5)
 console.log("after insertaion", myLinkedList);
-myLinkedList.printList()
-console.log("fined",myLinkedList.find(2));
+// myLinkedList.printList()
+// console.log("fined",myLinkedList.find(2));
+console.log(myLinkedList.pop());
+console.log("after deletion ", myLinkedList);
